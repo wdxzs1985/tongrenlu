32c2762f54bf13fa616d2714c3946bec2a40224b
wdxzs1985 <wdxzs1985@gmail.com>
2014-06-19 12:53:14 +0900

    user mapper

--- tongrenlu/src/main/java/info/tongrenlu/mapper/UserMapper.java
+++ tongrenlu/src/main/java/info/tongrenlu/mapper/UserMapper.java
@@ -2,16 +2,13 @@ package info.tongrenlu.mapper;
 
 import info.tongrenlu.domain.UserBean;
 
-import java.util.List;
 import java.util.Map;
 
 public interface UserMapper {
 
-    public void insert(UserBean userBean);
-
     public UserBean fetchBean(Map<String, Object> param);
 
-    public List<UserBean> fetchList(Map<String, Object> param);
+    public void insert(UserBean userBean);
 
     public void update(Map<String, Object> param);
 
