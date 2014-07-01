3de0792ece3a53b5a37778b4e2ac036343119bfa
wdxzs1985 <wdxzs1985@gmail.com>
2014-07-01 20:38:52 +0900

    tag

--- mysql-sql/V_TAG.sql
+++ mysql-sql/V_TAG.sql
@@ -6,14 +6,14 @@ VIEW `v_tag` AS
     select 
         `m_tag`.`id` AS `id`,
         `m_tag`.`tag` AS `tag`,
-        count(`r_article_tag`.`id`) AS `cnt`
+        count(`m_article`.`id`) AS `cnt`
     from
-        ((`r_article_tag`
-        left join `m_tag` ON ((`m_tag`.`id` = `r_article_tag`.`tag_id`)))
-        left join `m_article` ON ((`m_article`.`id` = `r_article_tag`.`article_id`)))
+        ((`m_tag`
+        left join `r_article_tag` ON (((`m_tag`.`id` = `r_article_tag`.`tag_id`)
+            and (`r_article_tag`.`del_flg` = 0))))
+        left join `m_article` ON (((`m_article`.`id` = `r_article_tag`.`article_id`)
+            and (`m_article`.`publish_flg` = 1)
+            and (`m_article`.`del_flg` = 0))))
     where
-        ((`m_article`.`del_flg` = 0)
-            and (`m_article`.`publish_flg` = 0)
-            and (`m_tag`.`del_flg` = 0)
-            and (`r_article_tag`.`del_flg` = 0))
-    group by `m_tag`.`id` , `m_tag`.`tag`
+        (`m_tag`.`del_flg` = 0)
+    group by `m_tag`.`id` , `m_tag`.`tag`;
