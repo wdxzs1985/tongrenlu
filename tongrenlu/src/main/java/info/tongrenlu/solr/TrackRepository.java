602800afbc3d8ed051673e705b4aab6e0ad1361a
wdxzs1985 <wdxzs1985@gmail.com>
2014-07-15 00:21:21 +0900

    solr query

--- tongrenlu/src/main/java/info/tongrenlu/solr/TrackRepository.java
+++ tongrenlu/src/main/java/info/tongrenlu/solr/TrackRepository.java
@@ -7,12 +7,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.solr.repository.Query;
 import org.springframework.data.solr.repository.SolrCrudRepository;
 
-public interface TrackRepository extends SolrCrudRepository<ArticleDocument, String> {
+public interface TrackRepository extends
+        SolrCrudRepository<ArticleDocument, String> {
 
     @Query(filters = "category:track")
     List<TrackDocument> findByArticleId(Integer articleId);
 
-    @Query(value = "xtag:?0 OR xtag:*?0*", filters = "category:track")
+    @Query(value = "xtag_ja:?0 OR xtag_ja:*?0* OR xtag_en:?0 OR xtag_en:*?0* OR xtag_zh:?0 OR xtag_zh:*?0*", filters = "category:track")
     Page<TrackDocument> findTrackOrderById(List<String> queries,
                                            Pageable pageable);
 
