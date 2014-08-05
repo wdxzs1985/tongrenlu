58d930e96218d5f46735e520fdb97c3c53b2f6d5
wdxzs1985 <wdxzs1985@gmail.com>
2014-08-05 23:55:27 +0900

    修复投稿详细页不显示标签

--- mysql-sql/r_track_rate.sql
+++ mysql-sql/r_track_rate.sql
@@ -4,6 +4,6 @@ CREATE TABLE `r_track_rate` (
   `track_id` int(11) NOT NULL,
   `rate` int(11) NOT NULL DEFAULT '3',
   `upd_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `del_flg` char(1) NOT NULL,
+  `del_flg` char(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
