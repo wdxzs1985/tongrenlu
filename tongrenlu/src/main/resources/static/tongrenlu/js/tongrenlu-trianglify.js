8c3d4b0abb4781447b59533764a70660975d2628
wdxzs1985 <wdxzs1985@gmail.com>
2014-09-04 01:23:17 +0900

    trianglify release

--- tongrenlu/src/main/resources/static/tongrenlu/js/tongrenlu-trianglify.js
+++ tongrenlu/src/main/resources/static/tongrenlu/js/tongrenlu-trianglify.js
@@ -1,20 +1,14 @@
 (function($){
 	$.fn.trianglify = function(options) {
-		var defaults = {};
+		var defaults = {
+			noiseIntensity: 0,
+			cellpadding: 0
+		};
 		
 		var settings = $.extend(defaults, options);
-		
-	    var t = new Trianglify({noiseIntensity: 0});
-	    if(settings.gradient) {
-	    	t.options.x_gradient = settings.gradient;
-	    } else {
-	    	t.options.x_gradient = Trianglify.randomColor();
-	    }
-	    
-	    t.options.y_gradient = t.options.x_gradient.map(function(c){return d3.rgb(c).brighter(.5)});
+	    var t = new Trianglify(settings);
 	    var pattern = t.generate(this.width(), this.outerHeight());
 	    this.css('background-image', pattern.dataUrl);
-	    
 	    return this;
 	}
 })(jQuery);
