7533e0f83969073c886ff944d1bda74e0e554f0e
wdxzs1985 <wdxzs1985@gmail.com>
2014-12-17 21:12:13 +0900

    timeline 显示日期

--- tongrenlu/src/main/java/info/tongrenlu/Application.java
+++ tongrenlu/src/main/java/info/tongrenlu/Application.java
@@ -11,18 +11,14 @@ import javax.servlet.SessionTrackingMode;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.ServletContextInitializer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.util.CookieGenerator;
 
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
+@SpringBootApplication
 @EnableTransactionManagement
 @ImportResource("classpath:spring-context.xml")
 public class Application implements ServletContextInitializer {
@@ -31,10 +27,11 @@ public class Application implements ServletContextInitializer {
         new SpringApplication(Application.class).run(args);
     }
 
-    private Log log = LogFactory.getLog(this.getClass());
+    private final Log log = LogFactory.getLog(this.getClass());
 
     @Override
-    public void onStartup(final ServletContext servletContext) throws ServletException {
+    public void
+            onStartup(final ServletContext servletContext) throws ServletException {
         this.log.info("setSessionTrackingModes: " + SessionTrackingMode.COOKIE);
         servletContext.setSessionTrackingModes(Collections.singleton(SessionTrackingMode.COOKIE));
     }
