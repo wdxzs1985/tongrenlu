502fd30594daccab5a1548bac16a6b2165097d0e
wdxzs1985 <wdxzs1985@gmail.com>
2014-12-24 15:42:22 +0900

    preview page with waterfall

---
+++ tongrenlu/src/main/resources/static/tongrenlu/preview/c87/data.js
@@ -0,0 +1,10 @@
+var data = [
+	{
+		title: 'Origin of Love',
+		artist: '発熱巫女〜ず',
+		mp3: 'http://jplayer.org/audio/mp3/Miaow-07-Bubble.mp3',
+		poster: 'http://hatsunetsumikos.net/oolv/images/oolv_banner.png',
+		span: '2',
+		style: 'height:100px'
+	}
+];
