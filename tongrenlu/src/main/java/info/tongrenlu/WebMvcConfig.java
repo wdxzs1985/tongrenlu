ee3ce81e385bea706390595b04572607907c86fe
wdxzs1985 <wdxzs1985@gmail.com>
2015-02-05 16:53:44 +0900

    error page

--- tongrenlu/src/main/java/info/tongrenlu/WebMvcConfig.java
+++ tongrenlu/src/main/java/info/tongrenlu/WebMvcConfig.java
@@ -1,5 +1,7 @@
 package info.tongrenlu;
 
+import info.tongrenlu.exception.ForbiddenException;
+import info.tongrenlu.exception.PageNotFoundException;
 import info.tongrenlu.interceptor.AccessInterceptor;
 import info.tongrenlu.interceptor.AdminAuthInterceptor;
 import info.tongrenlu.interceptor.AutoLoginInterceptor;
@@ -7,14 +9,20 @@ import info.tongrenlu.interceptor.ConsoleAuthInterceptor;
 import info.tongrenlu.interceptor.GuestAuthInterceptor;
 import info.tongrenlu.interceptor.TimerInterceptor;
 
+import java.util.Collections;
+
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -42,8 +50,7 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addResourceHandlers(final ResourceHandlerRegistry registry) {
         if (!registry.hasMappingForPattern("/files/**")) {
-            registry.addResourceHandler("/files/**")
-                    .addResourceLocations("file:" + this.outputPath);
+            registry.addResourceHandler("/files/**").addResourceLocations("file:" + this.outputPath);
         }
     }
 
@@ -62,22 +69,16 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addInterceptors(final InterceptorRegistry registry) {
-        registry.addInterceptor(this.timerInterceptor)
-                .addPathPatterns("/**")
-                .excludePathPatterns("/error");
-        registry.addInterceptor(this.accessInterceptor)
-                .addPathPatterns("/**")
-                .excludePathPatterns("/error");
+        registry.addInterceptor(this.timerInterceptor).addPathPatterns("/**").excludePathPatterns("/error");
+        registry.addInterceptor(this.accessInterceptor).addPathPatterns("/**").excludePathPatterns("/error");
         registry.addInterceptor(this.autoLoginInterceptor)
                 .addPathPatterns("/**")
                 .excludePathPatterns(WebMvcConfig.EXCLUDE_PATH);
         registry.addInterceptor(this.guestAuthInterceptor)
                 .addPathPatterns("/**")
                 .excludePathPatterns(WebMvcConfig.EXCLUDE_PATH);
-        registry.addInterceptor(this.consoleAuthInterceptor)
-                .addPathPatterns("/console/**");
-        registry.addInterceptor(this.adminAuthInterceptor)
-                .addPathPatterns("/admin/**");
+        registry.addInterceptor(this.consoleAuthInterceptor).addPathPatterns("/console/**", "/shop/**");
+        registry.addInterceptor(this.adminAuthInterceptor).addPathPatterns("/admin/**");
     }
 
     @Bean
@@ -88,4 +89,26 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
         return filter;
     }
 
+    @Autowired
+    private MessageSource messageSource = null;
+
+    @ExceptionHandler(PageNotFoundException.class)
+    public ModelAndView handlePageNotFoundException(final PageNotFoundException ex) {
+        String message = ex.getMessage();
+        if (StringUtils.isBlank(message)) {
+            message = this.messageSource.getMessage("error.pageNotFound", null, null);
+        }
+        final ModelAndView model = new ModelAndView("error", Collections.singletonMap("error", message));
+        return model;
+    }
+
+    @ExceptionHandler(ForbiddenException.class)
+    public ModelAndView handleForbiddenException(final ForbiddenException ex) {
+        String message = ex.getMessage();
+        if (StringUtils.isBlank(message)) {
+            message = this.messageSource.getMessage("error.forbidden", null, null);
+        }
+        final ModelAndView model = new ModelAndView("error", Collections.singletonMap("error", message));
+        return model;
+    }
 }
